eb76f748c75c9077c8879687739e19dccf1e9a2c
subhanmeer <banimeer@gmail.com>
2024-06-28 05:49:46 -0700

    change

--- app.js
+++ app.js
@@ -4,10 +4,13 @@ var path = require("path");
 var cookieParser = require("cookie-parser");
 var logger = require("morgan");
 const expressSession = require("express-session");
+// const os = require("os")
+// console.log(os.cpus().length);
 
 var indexRouter = require("./routes/index");
 var usersRouter = require("./routes/users");
 const passport = require("passport");
+const { log } = require("console");
 
 
 var app = express();
